7fa983305612df5ea9213a55932ae139f7900269
Adam <adam.palacz96@gmail.com>
2024-07-28 12:57:58 +0200

    Add data source for current client config, fix resource references in ML workspace module

--- IaC/modules/ml_workspace/main.tf
+++ IaC/modules/ml_workspace/main.tf
@@ -1,3 +1,5 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "ml_workspace_rg" {
   name     = var.resource_group_name
   location = var.location
@@ -5,24 +7,24 @@ resource "azurerm_resource_group" "ml_workspace_rg" {
 
 resource "azurerm_application_insights" "ml_workspace_ai" {
   name                = var.application_insights_name
-  location            = azurerm_resource_group.ml_workspace.location
-  resource_group_name = azurerm_resource_group.ml_workspace.name
+  location            = azurerm_resource_group.ml_workspace_rg.location
+  resource_group_name = azurerm_resource_group.ml_workspace_rg.name
   application_type    = "web"
 }
 
 resource "azurerm_key_vault" "ml_workspace_kv" {
   name                = var.key_vault_name
-  location            = azurerm_resource_group.ml_workspace.location
-  resource_group_name = azurerm_resource_group.ml_workspace.name
+  location            = azurerm_resource_group.ml_workspace_rg.location
+  resource_group_name = azurerm_resource_group.ml_workspace_rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = var.key_vault_sku_name
 }
 
 resource "azurerm_machine_learning_workspace" "ml_workspace" {
   name                    = var.machine_learning_workspace_name
-  location                = azurerm_resource_group.ml_workspace.location
-  resource_group_name     = azurerm_resource_group.ml_workspace.name
-  application_insights_id = azurerm_application_insights.ml_workspace.id
-  key_vault_id            = azurerm_key_vault.ml_workspace.id
+  location                = azurerm_resource_group.ml_workspace_rg.location
+  resource_group_name     = azurerm_resource_group.ml_workspace_rg.name
+  application_insights_id = azurerm_application_insights.ml_workspace_ai.id
+  key_vault_id            = azurerm_key_vault.ml_workspace_kv.id
   sku_name                = var.machine_learning_workspace_sku_name
 }
