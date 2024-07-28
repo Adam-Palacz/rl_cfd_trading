dbab97cda3abd81816adc388b5fd4572b64dc5b2
Adam <adam.palacz96@gmail.com>
2024-07-28 13:00:02 +0200

    Add storage account to ML workspace

--- IaC/modules/ml_workspace/main.tf
+++ IaC/modules/ml_workspace/main.tf
@@ -20,6 +20,14 @@ resource "azurerm_key_vault" "ml_workspace_kv" {
   sku_name            = var.key_vault_sku_name
 }
 
+resource "azurerm_storage_account" "ml_workspace_storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.ml_workspace_rg.name
+  location                 = azurerm_resource_group.ml_workspace_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_machine_learning_workspace" "ml_workspace" {
   name                    = var.machine_learning_workspace_name
   location                = azurerm_resource_group.ml_workspace_rg.location
@@ -27,4 +35,8 @@ resource "azurerm_machine_learning_workspace" "ml_workspace" {
   application_insights_id = azurerm_application_insights.ml_workspace_ai.id
   key_vault_id            = azurerm_key_vault.ml_workspace_kv.id
   sku_name                = var.machine_learning_workspace_sku_name
+  storage_account_id      = azurerm_storage_account.ml_workspace_storage.id
+  identity {
+    type = "SystemAssigned"
+  }
 }
